869089b5c22cd38dee699a8d88a393b45ddd9800
beecher <beechertrouble@gmail.com>
2014-09-25 16:07:07 -0400

    initial commit, all basically working

---
+++ README.md
@@ -0,0 +1,191 @@
+modalBasic
+========
+Trying to keep this one real bare bones and semantic - other things can extend it perhaps ...
+
+### Requirements
+- javascript must be enabled
+- jQuery
+
+### Browser Support
+- ie8 and lower may be funny with positioning as we're using a pseudo element to vertically center the modal content. 
+- Animations and transitions are accomplished with css, so your browser will need to support that, otherwise things will just show up without the fanciness. 
+
+### Namespacing
+- using a prefix of "_mb" in css
+- set as "window._mb" in js
+
+### Markup
+The following markup is appeneded to the document.body onInit :
+```html
+<div id="_mb_uber"> <!-- this provides the page overlay and fixed positioning -->
+	<div id="_mb_rel_wrap"> <!-- this is for veritcal centering with a :before element -->
+		<div id="_mb_content_wrap">
+			<div id="_mb_header"></div>
+			<div id="_mb_body"></div>
+			<div id="_mb_footer"></div>
+			<button id="_mb_close" type="button" title="click to close" aria-label="close modal">x</button>
+		</div>
+	</div>
+</div>
+```
+
+### Styling
+We are using css for proper positioning (not javascript), transitions, and of course styling. You can include the [default styles](https://raw.githubusercontent.com/beechertrouble/modalBasic/master/modalBasic.min.css), extend them, or roll your own.
+
+### Javascript
+
+#### Methods
+The following methods are available to you after init : 
+
+- `_mb.show(args);`
+	- advanced args : 
+		
+		```javascript
+			var args = {
+					header: 'header content with <span>markup</span>',
+					body: 'body content with <span>markup</span>, if you're into that kinda thing ...',
+					footer: 'footer content without markup, but this can take markup too.',
+					addClass : 'the_class_you_want_to_add_to_the_wrap or_classes',
+					showFor : 5000 // number of milliseconds to show the modal for, before hiding it automatically
+				};
+		```
+	- most basic example : 
+	
+		```javascript
+			_mb.show({body : 'hello'});
+		```
+- `_mb.hide();` - you guessed it.
+	- this is also called by, hitting the ESC key, clicking close button, or anything outside the content wrap.
+
+#### Callbacks / Events
+- There's a pile of callbacks that can be defined when you init the _mb. Most are passed :  a reference to the modal object, and some local vars for the main bits of markup.
+
+```javascript
+
+var args = {
+	callbacks: {
+		beforeInit : function() {
+			console.log('beforeInit');
+		},
+		onInit :  function(_mb, $wrap, $header, $body, $footer, $close) {
+			console.log('onInit');
+			console.log(_mb);
+			console.log($wrap);
+		},
+		beforeMarkup : function() {
+			console.log('beforeMarkup');
+		},
+		onMarkup :  function() {
+			console.log('onMarkup');
+		},
+		beforeBind : function() {
+			console.log('beforeBind');
+		},
+		onBind :  function() {
+			console.log('onBind');
+		},
+		beforeShow : function() {
+			console.log('beforeShow');
+		},
+		onShow :  function() {
+			console.log('onShow');
+		},
+		beforeHide :  function() {
+			console.log('beforeHide');
+		},
+		onHide :  function() {
+			console.log('onHide');
+		},
+		yarp : function() {
+			// this needs to be defined in the show(args)
+			console.log('yarp fires when the confirm button has been hit');
+		},
+		narp : function() {
+			// this needs to be defined in the show(args)
+			console.log('narp fires when the cancel button has been hit');
+		}
+	}
+};
+
+_mb.init(args);
+```
+
+### Confirm / Prompt
+- Based on the window.confirm method, but with greater styling and functional possibilities.
+- We're using a "yarp/narp" convention instead of "confirm/cancel".
+- Specify button text like (defaults to 'confirm' and 'cancel'):
+	 
+	 ```javascript
+	 var confrimArgs = {
+	 	yarp : {
+	 		text: "the confirm button text",
+	 		disabled: true // optional
+	 	},
+	 	narp : { 
+	 		text: { // if you want translations
+	 			"en" : "cancel in english",
+	 			"es" : "como se dice, 'cancel'?"
+	 		}
+	 	}
+	 };
+	 
+	 // then you'd pass it to the show method like
+	 _mb.show({
+	 		body: "whatever?",
+	 		confirm: confrimArgs
+	 	});
+	 ```
+- Specify functionality within the callbacks 'yarp' and 'narp' :
+	
+	```javascript
+	var confirmAction = function(_mb) {
+			// whatver you want, really ...
+		},
+		cancelAction = function(_mb) {
+			// whatver you want, really ...
+		};
+		
+	 _mb.show({
+	 		body: "whatever?",
+	 		callbacks: {
+	 			yarp : confirmAction,
+	 			narp : cancelAction
+	 		}
+	 	});
+	```
+
+
+### Translations
+- tries to grab the "lang" property from the html tag (defaults to "en"). (see ref for iso codes [here](http://www.w3schools.com/tags/ref_language_codes.asp))
+- you can manually override this: `_mb.state.lang = "jp";`
+- to use this feature you can:
+	- ignore it and just provide single strings.
+	- provide translation objects like :
+	
+		```javascript
+			var hello = {
+					"en" : "hello",
+					"es" : "hola",
+					"fr" : "bonjour"
+					// ... etc
+				};
+		```
+- you can call this method directly like :
+
+	```javascript
+		var hello = {
+				"en" : "hello",
+				"es" : "hola",
+				"fr" : "bonjour"
+				// ... etc
+			};
+		// auto lang choice ...
+		var helloTranslated = _mb.translate(hello);
+		// or override for aspecific lang ...
+		var helloFrance = _mb.translate(hello, 'fr');
+	```
+	
+## To Do
+- add translation to more places and make default text/arias more configurable
+- confirm button styles
+
---
+++ modalBasic.js
@@ -0,0 +1,319 @@
+/**
+*
+*
+* modalBasic
+* by BeecherTrouble : beechbot.com
+*
+*
+*/
+(function($, W) {
+	
+	var _mb = {
+			args : {
+				callbacks : {
+					beforeInit : undefined,
+					onInit : undefined,
+					beforeMarkup : undefined,
+					onMarkup : undefined,
+					beforeBind : undefined,
+					onBind : undefined,
+					beforeShow : undefined,
+					onShow : undefined,
+					beforeHide : undefined,
+					onHide : undefined,
+					yarp : undefined,
+					narp : undefined
+				}
+			},
+			state : {
+				lang : $("html").attr("lang") !== undefined ? $("html").attr("lang") : 'en',
+				inited : false,
+				markedup : false,
+				bound : false,
+				visible : false
+			}
+		},
+		$wrap, $header, $body, $footer, $close,
+		lastFocus,
+		hideClass = '_mb_hide_me',
+		showClass = '_mb_show_me',
+		doCallback, addMarkup, reset, buildConfirm, bind;
+			
+	
+	//
+	// the markup template ... let's expose this incase people want to override it
+	//
+	_mb.markup = '<div id="_mb_uber">'
+					+ '<div id="_mb_rel_wrap">'
+						+ '<div id="_mb_content_wrap">'
+							+ '<div id="_mb_header" class="' + hideClass + '"></div>'
+							+ '<div id="_mb_body"></div>'
+							+ '<div id="_mb_footer" class="' + hideClass + '"></div>'
+							+ '<button id="_mb_close" type="button" title="click to close" aria-label="close modal">x</button>' 
+						+ '</div>'
+					+ '</div>'
+				+ '</div>';
+				
+	_mb.confirmMarkup = '<div id="_mb_confirm_wrap">'
+							+ '<button id="_mb_confirm_narp" data-which="narp" type="button" aria-label="cancel">[narpText]</button>'
+							+ '<button id="_mb_confirm_yarp" data-which="yarp" type="button" aria-label="confirm">[yarpText]</button>'
+						+ '</div>';
+	
+	//
+	// some setup methods ...
+	//
+	_mb.init = function(args){
+		
+		//
+		// defaults
+		//
+		var args = args === undefined ? {} : args;
+		_mb.args.addCSS = args.addCSS !== undefined ? args.addCSS : true;
+		_mb.args.addMarkup = args.addMarkup !== undefined ? args.addMarkup : true;
+		if(args.callbacks !== undefined) {
+			for(var c in _mb.args.callbacks) {
+				_mb.args.callbacks[c] = args.callbacks[c] !== undefined ? args.callbacks[c] : undefined;
+			}
+		}
+		_mb.state.lang = args.lang !== undefined ? args.lang : _mb.state.lang;
+		
+		doCallback('beforeInit');
+
+		addMarkup();
+		bind();
+		
+		_mb.state.inited = true;
+		doCallback('onInit');
+		
+	}; // init()
+	
+	//
+	// some behavioral methods ... 
+	//
+	_mb.show = function(args) {
+		
+		if(!_mb.state.inited) return;
+		
+		lastFocus = document.activeElement;
+		lastFocus.blur();
+		
+		var addClass = '',
+			showFor = false,
+			confirm = false;
+		
+		if(args !== undefined) {
+			
+			reset();
+
+			if(args.header !== undefined)
+				$header.html(args.header).removeClass(hideClass);
+			
+			if(args.body !== undefined)
+				$body.html(args.body);
+
+			if(args.footer !== undefined)
+				$footer.html(args.footer).removeClass(hideClass);
+			
+			if(args.callbacks !== undefined) {
+				for(var c in args.callbacks) {
+					_mb.args.callbacks[c] = args.callbacks[c];
+				}
+			}
+			
+			if(args.confirm !== undefined)
+				buildConfirm(args.confirm);	
+
+			addClass = args.addClass !== undefined ? args.addClass : '';
+			showFor = args.showFor !== undefined ? args.showFor : false;
+
+		}
+
+		doCallback('beforeShow');
+		
+		$wrap
+			.removeClass()
+			.addClass(showClass + ' ' + addClass);
+
+		$body.focus();
+		
+		if($footer.find('button').length > -1)
+			$footer.find('button:enabled:first').focus();
+
+		if($body.find('form').length > -1) // autofocus on first element in form ...
+			$body.find('form:not([type="hidden"]) :input:visible:enabled:first').focus();
+		
+		_mb.state.visible = true;
+		
+		doCallback('onShow');
+
+		// showFor
+		if(!!showFor && !confirm)
+		 	setTimeout(function(){_mb.hide(); }, showFor);
+
+	}; // show()
+	_mb.hide = function() {
+		
+		if(!_mb.state.inited) return;
+		
+		doCallback('beforeHide');
+		doCallback('narp');
+		
+		$wrap.removeClass(showClass);
+		
+		_mb.state.visible = false;
+		
+		lastFocus.focus();
+		
+		doCallback('onHide');
+
+	}; // hide()
+	_mb.translate = function(ting, lang) {
+
+		if(ting === undefined || ting == '') return false;
+		
+		lang = lang !== undefined ? lang : _mb.state.lang;
+		
+		var original = ting
+			, translation = false;
+
+		if(typeof ting != 'object') {
+			ting = JSON.stringify(ting);
+			ting = JSON.parse(ting);
+		}
+
+		// try current lang ...
+		translation = ting[lang] === undefined ?  false : ting[lang];
+
+		// default to en because ...
+		if(!translation && _mb.state.lang != 'en')
+			translation = ting['en'] === undefined ?  false : ting['en'];
+
+		// still no? how about the original string ...
+		if(!translation && typeof original == 'string')
+			translation = original;
+
+		return translation;
+
+	}; // translate()
+		
+	//
+	// private methods ...
+	//
+	doCallback = function(callback) { 
+		callback = _mb.args.callbacks[callback];
+		if(typeof callback !== 'function') return;
+		if(!_mb.state.bound) {
+			callback(_mb); 
+		} else {
+			callback(_mb, $wrap, $header, $body, $footer, $close); 
+		}
+	};
+	addMarkup = function(){
+		
+		if(!_mb.args.addMarkup || _mb.state.markedup) return false;
+		
+		doCallback('beforeMarkup');
+		
+		$(document.body).append(_mb.markup);
+		
+		_mb.state.markedup = true;
+		
+		doCallback('onMarkup');
+		
+	}; // addMarkup()
+	reset = function() {
+	
+		$body.empty();
+		$header.addClass(hideClass).empty();
+		$footer.addClass(hideClass).empty();
+		_mb.args.callbacks.narp = undefined;
+		_mb.args.callbacks.yarp = undefined;
+		
+	}; // reset()
+	buildConfirm = function(confirm) {
+
+		var defaults = { // leaving space for translation
+				yarp : {
+					'en' : 'confirm'
+				},
+				narp : {
+					'en' : 'cancel'
+				}
+			},
+			yarpText = _mb.translate(confirm.yarp.text) !== undefined ? confirm.yarp.text : defaults.yarp, // ✓
+			narpText = _mb.translate(confirm.narp.text) !== undefined ? confirm.narp.text : defaults.narp; // ✗
+
+		yarpText = _mb.translate(yarpText);
+		narpText = _mb.translate(narpText);
+
+		$footer
+			.append(_mb.confirmMarkup.replace('[yarpText]', '<span>✓</span> ' + yarpText).replace('[narpText]', '<span>✗</span> ' + narpText));
+
+		if(confirm.yarp !== undefined && confirm.yarp.disabled)
+			$("#_mb_confirm_yarp").attr('disabled', true);
+
+		if(confirm.narp !== undefined && confirm.narp.disabled)
+			$("#_mb_confirm_narp").attr('disabled', true);
+
+		$footer.removeClass(hideClass);
+
+	}; // buildConfirm()
+	bind = function() {
+		
+		// refs ...
+		$wrap = $('#_mb_uber');
+		$header = $("#_mb_header");
+		$body = $("#_mb_body");
+		$footer = $("#_mb_footer");
+		$close = $("#_mb_close");
+		
+		doCallback('beforeBind');
+		
+		$(document.body)
+			.on('click', '#_mb_uber', function(e) {
+				e.preventDefault();
+				e.stopPropagation();
+				_mb.hide();
+			})
+			.on('click', '#_mb_content_wrap', function(e) {
+				e.stopPropagation();
+			})
+			.on('click', '#_mb_close', function(e) {
+				e.preventDefault();
+				_mb.hide();
+			})
+			.on('click', '#_mb_confirm_wrap button', function(e) {
+				e.preventDefault();
+				
+				var which = $(this).data('which');
+				doCallback(which);
+				
+				if(which == 'yarp');
+					_mb.args.callbacks.narp = undefined;
+					
+				_mb.hide();
+
+			});
+		
+		$(W)
+			.on('keyup', function(e){
+
+				if(e.which == 27 && _mb.state.visible) _mb.hide();
+
+			});
+		
+		_mb.state.bound = true;
+			
+		doCallback('onBind');
+		
+	}; // bind()
+	
+	W._mb = _mb;
+
+})($ || jQuery, window); // modalBasic	
+
+/**
+* Expose as an AMD
+*/
+if (typeof define === "function") 
+	define("_mb", [], function () { return _mb; } );
